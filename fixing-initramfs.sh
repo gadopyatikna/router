4b854c50e5b383e0e45fdec7f4b31d850f23e528
alex <5575343+gadopyatikna@users.noreply.github.com>
2026-03-22 12:23:52 +0100

    Fix permission errors and add image creation commands
    Fixed permission issues when copying resolv.conf and added commands to create and write to rpi.img.

--- fixing-initramfs.sh
+++ fixing-initramfs.sh
@@ -4,13 +4,13 @@ sudo mount --bind /sys /mnt/rpi/sys
 sudo chroot /mnt/rpi
 
 sudo apt update
-# sudo: unable to resolve host alex-QEMU-Virtual-Machine: Temporary failure in name resolution
 #sudo: unable to allocate pty: No such device
-exit
-
 cp /etc/resolv.conf /mnt/rpi/etc/resolv.conf
-cp: cannot create regular file '/mnt/rpi/etc/resolv.conf': Permission denied
 sudo cp /etc/resolv.conf /mnt/rpi/etc/resolv.conf
 sudo mount -t devpts devpts /mnt/rpi/dev/pts
 sudo chroot /mnt/rpi
 sudo apt update
+
+# tmp disk to fix 
+sudo dd if=/dev/disk4 of=rpi.img status=progress
+sudo dd if=rpi.img of=/dev/rdisk4 bs=4m status=progress
